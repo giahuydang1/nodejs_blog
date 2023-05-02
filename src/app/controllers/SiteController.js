dde71b5c5f36e928e9248ff17afbd7db5d30aef3
Huy <danggiahuy147@gmail.com>
2023-05-02 18:13:38 +0700

    [CRUD] Read - detail page

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,10 +1,15 @@
 const Course = require('../models/Course')
+const { mutipleMongooseToObject } = require('../../util/mongoose');
 
 class SiteController {
     // [GET] /
     index(req, res, next) {
         Course.find({})
-            .then(courses => res.json(courses))
+            .then((courses) => {
+                res.render('home', {
+                    courses: mutipleMongooseToObject(courses),
+                });
+            })
             .catch(next);
         // res.render('home');
     }
