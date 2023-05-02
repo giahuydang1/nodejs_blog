8c98999dcf9a7c5a3b6024f3d9c06e3f23c818b9
Huy <danggiahuy147@gmail.com>
2023-05-02 17:09:12 +0700

    connect db

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,7 +1,12 @@
+const Course = require('../models/Course')
+
 class SiteController {
     // [GET] /
-    index(req, res) {
-        res.render('home');
+    index(req, res, next) {
+        Course.find({})
+            .then(courses => res.json(courses))
+            .catch(next);
+        // res.render('home');
     }
 
     // [GET] /search
