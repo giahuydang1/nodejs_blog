6a81bb23fa7b79ce676519e2c2f08692a1734458
Huy <danggiahuy147@gmail.com>
2023-05-02 15:27:37 +0700

    routes & controllers

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const handlebars = require('express-handlebars').engine
 const app = express()
 const port = 3000
 
+const route = require('./routes')
+
 app.use(morgan('combined'))
 
 // Use static folder
@@ -22,21 +24,8 @@ app.engine('hbs', handlebars({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'))
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
-
-app.get('/news', (req, res) => {
-  res.render('news')
-})
-
-app.get('/search', (req, res) => {
-  res.render('search')
-})
-
-app.post('/search', (req, res) => {
-  res.send('')
-})
+// Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
